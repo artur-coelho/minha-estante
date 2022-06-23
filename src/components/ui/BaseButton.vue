ba060f71c776f3de0b31f751b65e0f3588d7dbd4
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-06-23 01:44:09 -0300

    created BaseButton component

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,81 @@
+<template>
+  <button v-if="!link" :class="[size, variant, display]">
+    <slot></slot>
+  </button>
+  <router-link v-else :to="to" :class="[size, variant, block]">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+  export default {
+    props: {
+      size: { type: String, required: false, default: 'small' },
+      variant: { type: String, required: false, default: 'primary' },
+      display: { type: String, required: false, default: 'flex' },
+      link: { type: Boolean, required: false, default: false },
+      to: { type: String, required: false, default: '/' },
+    },
+  };
+</script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Archivo:wght@600;700&display=swap');
+
+  button,
+  a {
+    text-decoration: none;
+    font: inherit;
+    font-family: Archivo, sans-serif;
+    color: white;
+    cursor: pointer;
+    border: none;
+    border-radius: 8px;
+    margin-right: 0.5rem;
+    display: inline-block;
+  }
+
+  a:hover,
+  a:active,
+  button:hover,
+  button:active {
+    background-color: #1e51ba;
+    border-color: #1e51ba;
+  }
+
+  .primary {
+    background-color: #4a80f0;
+  }
+
+  .orange {
+    background-color: #f2994a;
+  }
+
+  .small {
+    padding: 1rem 2rem;
+    font-weight: 600;
+    font-size: 16px;
+  }
+
+  .big {
+    width: 250px;
+    height: 100px;
+    padding: 2rem 4rem;
+    font-weight: 700;
+    font-size: 26px;
+  }
+
+  .orange:hover,
+  .orange:active {
+    background-color: #d17728;
+  }
+
+  .flex {
+    display: inline;
+  }
+
+  .block {
+    display: block !important;
+    width: 100% !important;
+  }
+</style>
