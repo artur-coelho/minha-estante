031dbc6cc1d8cf7fd7634338738c52751a5756c4
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-07-19 21:52:33 -0300

    created form card ui component

---
+++ src/components/ui/FormCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+    <div v-if="title" class="form-header">
+      <h3 class="form-title">{{ title }}</h3>
+      <hr />
+    </div>
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: { type: String, required: false, default: '' },
+    },
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<style scoped>
+  hr {
+    border: 1px solid #e6e6f0;
+  }
+  .card {
+    background: #ffffff;
+    border: 1px solid #e6e6f0;
+    border-radius: 8px;
+    padding: 2rem 104px;
+    margin: 0 auto;
+    max-width: 80%;
+  }
+  .form-header {
+    margin-bottom: 4rem;
+  }
+  .form-title {
+    margin-bottom: 20px;
+
+    font-family: 'Archivo', sans-serif;
+    font-weight: 600;
+    font-size: 24px;
+    color: #32264d;
+  }
+</style>
